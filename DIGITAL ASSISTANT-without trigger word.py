1721e59185023f8af107bc6f861543125be63508
Ganeshbharathi <65957447+Ganeshbharathi@users.noreply.github.com>
2020-06-09 18:15:00 +0530

    Update DIGITAL ASSISTANT-without trigger word.py

--- DIGITAL ASSISTANT-without trigger word.py
+++ DIGITAL ASSISTANT-without trigger word.py
@@ -192,7 +192,7 @@ def cloudy(cld):
    
     
     degree_sign = u'\N{DEGREE SIGN}'
-    owm = pyowm.OWM('736bd9318505d3b64f42791bb11f0781')
+    owm = pyowm.OWM('your API kEY')
     observation = owm.weather_at_place(f'{cld},India')
     weather = observation.get_weather()
     temperature = weather.get_temperature('celsius')['temp']
